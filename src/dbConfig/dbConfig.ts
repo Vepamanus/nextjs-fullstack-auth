3fecd8e12a845b87f340cdb3e68fa386a6f471bd
vepamanusrn <iamsrinu.vepamanu@gmail.com>
2023-11-28 22:43:32 +0530

    some files modified

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,24 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connect() {
-    try {
-        mongoose.connect(process.env.MONGO_URI!);
-        const connection = mongoose.connection;
+  try {
+    await mongoose.connect(
+      "mongodb+srv://vepamanusv:vepamanusv200@cluster0.tf8f9jk.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully');
-        })
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
 
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-            process.exit();
-        })
-
-    } catch (error) {
-        console.log('Something goes wrong!');
-        console.log(error);
-        
-    }
-
-
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. Please make sure MongoDB is running. " + err
+      );
+      process.exit(1); // Exit with a failure code
+    });
+  } catch (error) {
+    console.log("Something goes wrong!");
+    console.error(error);
+    process.exit(1); // Exit with a failure code
+  }
 }
